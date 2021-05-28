2b0fd7e3eaad24a436b716418e2ae179f5434398
jjongp912 <jjongp912@naver.com>
2021-05-28 17:55:59 +0900

    "NaN"을 체크하고 연산 수행

---
+++ Java_develpMonster_PT/src/thisis/java/chap03/InputDataCheckNaNExample2.java
@@ -0,0 +1,23 @@
+package thisis.java.chap03;
+
+public class InputDataCheckNaNExample2 {
+
+	public static void main(String[] args) {
+		String userInput = "NaN"; 
+		double val = Double.valueOf(userInput);
+		
+		double currentBalance = 10000.0;
+		
+		if (Double.isNaN(val)) {
+			System.out.println("NaN이 입력되어 처리할 수 없음");
+			// if 조건식을 넣어서 val의 값이 NaN인지를 검사하여 막아준다.
+			
+			val = 0.0;
+		}
+		
+		currentBalance += val;
+		System.out.println(currentBalance);
+		// currentBalance 값이 연산 후에도 보존된다.
+	}
+
+}
