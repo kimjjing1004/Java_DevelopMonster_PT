d96c51b80b924baaca067363bc3e8604058b3846
jjongp912 <jjongp912@naver.com>
2021-05-31 21:30:46 +0900

    1부터 10까지 출력

---
+++ Java_develpMonster_PT/src/thisis/java/chap04/WhilePrintFrom1To10Example.java
@@ -0,0 +1,15 @@
+package thisis.java.chap04;
+
+public class WhilePrintFrom1To10Example {
+
+	public static void main(String[] args) {
+		int i = 1;
+		while (i <= 10) {
+			System.out.println(i);
+			i++;
+			// while문으로 1 ~ 10까지 출력
+		}
+
+	}
+
+}
