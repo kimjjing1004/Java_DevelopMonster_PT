4ca237052f4a1440b38f8e2ded7422f6a904bb77
jjongp912 <jjongp912@naver.com>
2021-06-01 17:42:38 +0900

    바깥쪽 반복문 종료

--- Java_develpMonster_PT/src/thisis/java/chap04/BreakOutterExample.java
+++ Java_develpMonster_PT/src/thisis/java/chap04/BreakOutterExample.java
@@ -1,4 +1,4 @@
-package thisis.java.chap04;
+ package thisis.java.chap04;
 
 public class BreakOutterExample {
 
@@ -8,8 +8,8 @@ public class BreakOutterExample {
 				System.out.println(upper + "-" + lower);
 				if (lower == 'g') {
 					break Outter;
-				}
-			}
+				} // 반복문이 중첩 되있을 경우 break문은 가장 가까운 반복문만 종료 하기 때문에 
+			}		// 중첩된 반복문에서 바깥문 반복문까지 종료 시키려면 바깥쪽 반복문에 이름을 붙이고 break 이름; 사용!
 		}
 		System.out.println("프로그램 실행 종료");
 	
