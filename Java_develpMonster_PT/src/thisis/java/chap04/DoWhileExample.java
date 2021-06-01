0551b3457520c6cc926452767222bf4805eae11a
jjongp912 <jjongp912@naver.com>
2021-06-01 17:41:33 +0900

    do-while문

--- Java_develpMonster_PT/src/thisis/java/chap04/DoWhileExample.java
+++ Java_develpMonster_PT/src/thisis/java/chap04/DoWhileExample.java
@@ -16,6 +16,7 @@ public class DoWhileExample {
 			inputString = scanner.nextLine();	//	키보드로 입력한 문자열을 얻음
 			System.out.println(inputString);
 		} while (! inputString.equals("q"));	//	문자열을 비교할 때는 equals() 메소드를 사용
+			// 문자열이 q와 같냐  // ; 꼭 써주자
 		
 		System.out.println();
 		System.out.println("프로그램 종료");
