38f8396ae41df74b84363dbe50e4a9df5f8e7af9
jjongp912 <jjongp912@naver.com>
2021-06-09 21:59:32 +0900

    싱글톤

---
+++ Java_develpMonster_PT/src/thisis/java/chap06/Singleton.java
@@ -0,0 +1,14 @@
+package thisis.java.chap06;
+
+public class Singleton {
+	private static Singleton singleton = new Singleton();
+	
+	private Singleton() {
+		
+	}
+	
+	static Singleton getInstance() {
+		return singleton;
+	}
+
+}
