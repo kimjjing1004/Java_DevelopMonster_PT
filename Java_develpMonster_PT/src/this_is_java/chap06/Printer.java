9ff91741271897664bbff64b8d21f3c4bcfd1892
jjongp912 <jjongp912@naver.com>
2021-06-14 13:51:11 +0900

    연습문제

--- Java_develpMonster_PT/src/this_is_java/chap06/Printer.java
+++ Java_develpMonster_PT/src/this_is_java/chap06/Printer.java
@@ -1,6 +1,26 @@
 package this_is_java.chap06;
 
 public class Printer {
+	
+	// 리턴값이 없는 메소드(void)	// 메소드 오버로딩
+	void println(int value) {
+		System.out.println(value);
+	}
+	
+	void println(boolean value) {
+		System.out.println(value);
+	}
+	
+	void println(double value) {
+		System.out.println(value);
+	}
+	
+	void println(String value) {
+		System.out.println(value);
+	}
+	
+	// 정적 메소드
+	/*
 	static void println(int value) {
 		System.out.println(value);
 	}
@@ -16,5 +36,5 @@ public class Printer {
 	static void println(String value) {
 		System.out.println(value);
 	}
-
+	*/
 }
--- Java_develpMonster_PT/src/this_is_java/chap06/PrinterExample.java
+++ Java_develpMonster_PT/src/this_is_java/chap06/PrinterExample.java
@@ -3,11 +3,21 @@ package this_is_java.chap06;
 public class PrinterExample {
 
 	public static void main(String[] args) {
+		// 객체 외부에서 메소드 호출	// 메소드 오버로딩
+		Printer printer = new Printer();
+		
+		printer.println(10);
+		printer.println(true);
+		printer.println(5.7);
+		printer.println("홍길동");
+		
+		// 정적 멤버 사용
+		/*
 		Printer.println(10);
 		Printer.println(true);
 		Printer.println(5.7);
 		Printer.println("홍길동");
-
+		*/
 	}
 
 }
