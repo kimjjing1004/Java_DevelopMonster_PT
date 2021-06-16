e4b1b11a68eec3aa39a94bb7a94e6a6e4069c50b
jjongp912 <jjongp912@naver.com>
2021-06-16 13:23:59 +0900

    부모 클래스

---
+++ Java_develpMonster_PT/src/this_is_java/chap07/People.java
@@ -0,0 +1,12 @@
+package this_is_java.chap07;
+
+public class People {
+	public String name;
+	public String ssn;
+	
+	public People(String name, String ssn) {
+		this.name = name;
+		this.ssn = ssn;
+	}
+
+}
