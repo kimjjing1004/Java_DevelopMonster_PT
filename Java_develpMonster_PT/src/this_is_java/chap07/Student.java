678bf1a9d03234b63305895bf1ff6eb467431a30
jjongp912 <jjongp912@naver.com>
2021-06-16 13:24:23 +0900

    자식 클래스

---
+++ Java_develpMonster_PT/src/this_is_java/chap07/Student.java
@@ -0,0 +1,11 @@
+package this_is_java.chap07;
+
+public class Student extends People {
+	public int studentNo;
+	
+	public Student(String name, String ssn, int studentNo) {
+		super(name, ssn);	//	부모 생성자 호출
+		this.studentNo = studentNo;
+	}
+
+}
