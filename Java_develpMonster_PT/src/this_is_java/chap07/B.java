02c528fe2d16f55fcf0027980490073aa65b821b
jjongp912 <jjongp912@naver.com>
2021-06-18 17:59:48 +0900

    protected 접근 제한자 테스트

---
+++ Java_develpMonster_PT/src/this_is_java/chap07/B.java
@@ -0,0 +1,10 @@
+package this_is_java.chap07;
+
+public class B {
+	public void method() {
+		A a = new A();		//	(o)
+		a.field = "value";	//	(o)
+		a.method();
+	}
+
+}
---
+++ Java_develpMonster_PT/src/this_is_java/chap07/protectedEx/C.java
@@ -0,0 +1,12 @@
+package this_is_java.chap07.protectedEx;
+
+import this_is_java.chap07.A;
+
+public class C {
+	public void method() {
+//		A a = new A();		//	(x)
+//		a.field = "value";	//	(x)
+//		a.method();			//	(x)
+	}
+	
+}
