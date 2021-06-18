dd0ccaf6c42f39abaf9959997336b685b0004ab7
jjongp912 <jjongp912@naver.com>
2021-06-18 18:01:37 +0900

    자동 타입 변환 후의 멤버 접근

---
+++ Java_develpMonster_PT/src/this_is_java/chap07/Child.java
@@ -0,0 +1,13 @@
+package this_is_java.chap07;
+
+public class Child extends Parent {
+
+	@Override	//	재정의
+	public void method2() {
+		System.out.println("Child-method2()");
+	}
+	public void method3() {
+		System.out.println("Child-method3()");
+	}			//	재정의
+
+}
---
+++ Java_develpMonster_PT/src/this_is_java/chap07/Parent.java
@@ -0,0 +1,12 @@
+package this_is_java.chap07;
+
+public class Parent {
+	public void method1() {
+		System.out.println("Parent-method1()");
+	}
+	
+	public void method2() {
+		System.out.println("Parent_method2()");
+	}
+	
+}
