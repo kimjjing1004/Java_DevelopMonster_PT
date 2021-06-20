cead3e3d3c11ea78851a7d7fbd9e9e9905c7b3b9
Sexpistols <Sexpistols@DESKTOP-NV4A86E.mshome.net>
2021-06-21 00:23:38 +0900

    실행 클래스

---
+++ Java_develpMonster_PT/src/this_is_java/chap07/DriverExample.java
@@ -0,0 +1,14 @@
+package this_is_java.chap07;
+
+public class DriverExample {
+	public static void main(String[] args) {
+		Driver driver = new Driver();
+		
+		Bus bus = new Bus();
+		Taxi taxi = new Taxi();
+		
+		driver.drive(bus);			//	자동 타입 변환: Vehicle vehicle = bus;
+		driver.drive(taxi);			//	자동 타입 변환: Vehicle vehicle = taxi;
+	}
+
+}
