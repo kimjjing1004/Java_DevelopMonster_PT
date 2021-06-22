355a96f261193e1686f72aed78d88b7a7dbfeb80
jjongp912 <jjongp912@naver.com>
2021-06-22 18:10:35 +0900

    추상 메소드 선언

---
+++ Java_develpMonster_PT/src/this_is_java/chap07/Animal.java
@@ -0,0 +1,12 @@
+package this_is_java.chap07;
+
+public abstract class Animal {
+	public String kind;
+	
+	public void breathe() {
+		System.out.println("숨을 쉽니다.");
+	}
+	
+	public abstract void sound();
+
+}
