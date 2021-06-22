f65ff5307e64a203a1aaf11a8a97fe4cdd2e3218
jjongp912 <jjongp912@naver.com>
2021-06-22 18:22:23 +0900

    실행 클래스

---
+++ Java_develpMonster_PT/src/this_is_java/chap07/AnimalExample.java
@@ -0,0 +1,30 @@
+package this_is_java.chap07;
+
+public class AnimalExample {
+	public static void main(String[] args) {
+		Dog dog = new Dog();
+		Cat cat = new Cat();
+		dog.sound();
+		cat.sound();
+		System.out.println("-----");
+		
+		//	변수의 자동 타입 변환
+		Animal animal = null;
+		
+		animal = new Dog();					//	자동 타입 변환
+		animal.sound();						//	재정의된 메소드 호출
+		
+		animal = new Cat();					//	자동 타입 변환
+		animal.sound();						//	재정의된 메소드 호출
+		System.out.println("-----");
+		
+		//	메소드의 다형성
+		animalSound(new Dog());				//	자동타입변환 new Dog()--animal
+		animalSound(new Cat());				//	자동타입변환 new Cat()--Animal
+	}
+	
+	public static void animalSound(Animal animal) {
+		animal.sound();						//	재정의된 메소드 호출
+	}
+
+}
