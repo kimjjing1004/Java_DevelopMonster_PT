226fcefaa96b830b7b3db54823aa5eddcfd0d7bd
jjongp912 <jjongp912@naver.com>
2021-07-07 21:29:00 +0900

    기존 구현 클래스

---
+++ Java_develpMonster_PT/src/this_is_java/chap08/MyClassA.java
@@ -0,0 +1,12 @@
+package this_is_java.chap08;
+
+public class MyClassA implements MyInterface {
+
+	@Override
+	public void method1() {
+		System.out.println("MyClassA-method1() 실행");
+		
+	}
+	
+
+}
