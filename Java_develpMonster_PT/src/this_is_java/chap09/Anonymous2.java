969c57bd753ea6e9a571aab6ecc5046c2c9acf4b
jjongp912 <jjongp912@naver.com>
2021-07-19 21:58:38 +0900

    익명 구현 클래스와 객체 생성

---
+++ Java_develpMonster_PT/src/this_is_java/chap09/Anonymous2.java
@@ -0,0 +1,45 @@
+package this_is_java.chap09;
+
+public class Anonymous2 {
+	// 필드 초기값으로 대입
+	RemoteControl field = new RemoteControl() {
+
+		@Override
+		public void turnOn() {
+			System.out.println("TV를 켭니다.");		//	필드 선언과 초기값 대입
+			
+		}
+
+		@Override
+		public void turnOff() {
+			System.out.println("TV를 끕니다.");
+			
+		}
+		
+	};
+	
+	void method1() {
+		// 로컬 변수값으로 대입
+		RemoteControl localVar = new RemoteControl() {
+
+			@Override
+			public void turnOn() {
+				System.out.println("Audio를 켭니다.");	//	로컬 변수 선언과 초기값 대입
+				
+			}
+
+			@Override
+			public void turnOff() {
+				System.out.println("Audio를 끕니다.");
+				
+			}
+			
+		};
+		//	로컬 변수 사용
+		localVar.turnOn();
+	}
+	
+	void method2(RemoteControl rc) {
+		rc.turnOn();
+	}
+}
