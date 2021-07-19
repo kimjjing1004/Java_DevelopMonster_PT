6d96ac7ceb579417294746ea09269b2b938b519e
jjongp912 <jjongp912@naver.com>
2021-07-19 21:59:40 +0900

    실행 클래스

---
+++ Java_develpMonster_PT/src/this_is_java/chap09/Main2.java
@@ -0,0 +1,10 @@
+package this_is_java.chap09;
+
+public class Main2 {
+	public static void main(String[] args) {
+		Window w = new Window();
+		w.button1.touch();		//	버튼 클릭
+		w.button2.touch();
+	}
+
+}
