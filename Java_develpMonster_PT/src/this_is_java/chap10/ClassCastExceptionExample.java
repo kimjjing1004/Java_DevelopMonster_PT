94370374c88150157598237b484a26ebcc59cac6
jjongp912 <jjongp912@naver.com>
2021-07-24 18:18:56 +0900

    ClassCastException

---
+++ Java_develpMonster_PT/src/this_is_java/chap10/ClassCastExceptionExample.java
@@ -0,0 +1,30 @@
+package this_is_java.chap10;
+
+public class ClassCastExceptionExample {
+	public static void main(String[] args) {
+		Dog dog = new Dog();
+		changeDog(dog);
+		
+		Cat cat = new Cat();
+		changeDog(cat);
+	}
+	
+	public static void changeDog(Animal animal) {
+//		if (animal instanceof Dong) {
+			Dog dog = (Dog) animal;		//	ClassCastException 발생 가능
+//		}
+	}
+
+}
+
+class Animal {
+	
+}
+
+class Dog extends Animal {
+	
+}
+
+class Cat extends Animal {
+	
+}
