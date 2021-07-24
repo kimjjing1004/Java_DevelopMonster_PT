280d7108f2d5718d66b592f3e9bada67dacb84a8
jjongp912 <jjongp912@naver.com>
2021-07-24 18:16:13 +0900

    NullPointerException이 발생하는 경우

---
+++ Java_develpMonster_PT/src/this_is_java/chap10/NullPointerExceptionExample.java
@@ -0,0 +1,9 @@
+package this_is_java.chap10;
+
+public class NullPointerExceptionExample {
+	public static void main(String[] args) {
+		String data = null;
+		System.out.println(data.toString());
+	}
+
+}
