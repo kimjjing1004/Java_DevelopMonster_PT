88102b94e7e8c65e515c4030f6f4725c7f086d15
kimjjing1004 <kimjjing1004@naver.com>
2021-08-03 00:05:56 +0900

    객체 동등 비교(equals() 메소드)

---
+++ Java_develpMonster_PT/src/this_is_java/chap11/Member.java
@@ -0,0 +1,26 @@
+package this_is_java.chap11;
+
+public class Member {
+	public String id;
+	
+	public Member(String id) {
+		this.id = id;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (obj instanceof Member) {		//	매개값이 Member 타입인지 확인
+			Member member = (Member) obj;	//	Member 타입으로 강제 타입 변환하고
+			if (id.equals(member.id)) {		//	id 필드값이 동일한지 검사한 후,
+				return true;				//	동일하다면 true를 리턴
+			}
+		}
+		return false;						//	매개값이 Member 타입이 아니거나
+	}										//	id 필드값이 다른 경우 false를 리턴
+
+	@Override
+	public int hashCode() {
+		return id.hashCode();				//	id가 동일한 문자열인 경우 같은 해시 코드를 리턴
+	}
+	
+}
